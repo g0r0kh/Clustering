20500f2d42eeeaa3078423c8fed19c6ebf18e718
g0r0kh <gorokhovvivan09@gmail.com>
2024-09-06 12:16:07 +0300

    libs disaccordance solved

--- base.py
+++ base.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
-from louis import plain_text
+# from louis import plain_text
 from scipy.spatial.distance import pdist
 from scipy.cluster.hierarchy import *
 from matplotlib import pyplot as plt
 from matplotlib import rc
 from sklearn.cluster import KMeans
 
-#
-# xlsx source import
+
+
+
 data = pd.read_excel('sample_data.xlsx',
                      sheet_name='sheet1')
 
